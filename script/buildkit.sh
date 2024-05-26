3b8018ce751a4d75c9de8bb087fd9fff53267437
Jia Jia <angersax@sina.com>
2024-05-26 19:56:59 +0800

    chore(build): bump tools version

--- script/buildkit.sh
+++ script/buildkit.sh
@@ -2,7 +2,7 @@
 
 set -eu -o pipefail
 
-BUILDKIT_VERSION=0.12.0
+BUILDKIT_VERSION=0.13.0
 
 curl -sfL https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.linux-amd64.tar.gz -o buildkit.tar.gz
 sudo tar -zxvf buildkit.tar.gz -C /usr/local/
